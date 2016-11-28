7dd3b7cfcd614222874fe3b295e1da0c730a61fc
Roy Scheffers <r.scheffers@gmail.com>
2016-11-28 15:38:04 -0500

    Add support for change name action

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -7,6 +7,16 @@ var reducer = (state = {name: 'anonymous'}, action) => {
   // the above is ES6 syntax, below ES5.
   // If no state is provided, set an object, else state = state
   // state = state || {name: 'Anonymouse'};
+  console.log('New action', action);
+  switch (action.type) {
+    case 'CHANGE_NAME':
+      return {
+          ...state,
+          name: action.name
+      };
+    default:
+      return state;
+  }
 
   return state;
 };
@@ -15,3 +25,12 @@ var store = redux.createStore(reducer);
 
 var currentState = store.getState();
 console.log('currentState ', currentState);
+
+var action = {
+  // only requirement is type, it resembles the action name
+  type: 'CHANGE_NAME',
+  name: 'Roy Scheffers'
+};
+
+store.dispatch(action);
+console.log('Name should be Roy', store.getState() );
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -2,24 +2,39 @@ var redux = require('redux');
 
 console.log('Starting Redux Todo example');
 
+
+// ______________________________________________
+//
+//                    Reducer
+// ______________________________________________
+
 var stateDefault = {
     searchText: '',
     showCompleted: false,
     todos: []
 }
 var reducer = (state = stateDefault, action) => {
+  switch (action.type) {
+    case 'CHANGE_SEARCH_TEXT':
+      return {
+        ...state,
+        searchText: action.searchText
+      };
+    default:
+      return state;
+  }
 return state;
 };
-
 var store = redux.createStore(reducer);
 
-var currentState = store.getState();
-console.log('Current State of the store', currentState);
 
-// Assignment:
-// - Creaet a store
-// - Create a reduces
-//   Default value:
-//     searchText, default to ''
-//     showCompleted, default to false
-//     todos, default []
+console.log('Initial state of the store', store.getState() );
+
+
+store.dispatch({
+  // only requirement is type, it resembles the action name
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'Roy'
+});
+
+console.log('searchText should be "Roy"', store.getState() );
