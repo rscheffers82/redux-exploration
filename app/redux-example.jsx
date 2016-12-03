0f17a47a23a8ccb404a7ccbec3f3bbab627ccd32
Roy Scheffers <r.scheffers@gmail.com>
2016-12-02 19:39:15 -0500

    Add actiosn for adding/removing hobbies and movies

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,21 +2,60 @@ var redux = require('redux');
 
 console.log('Starting redux example');
 
-
-var reducer = (state = {name: 'anonymous'}, action) => {
+var defaultState = {
+  name: 'anonymous',
+  hobbies: [],
+  movies: []
+};
+var nextHobbyId = 1;
+var nextMovieId = 1;
+var reducer = (state = defaultState, action) => {
   // the above is ES6 syntax, below ES5.
   // If no state is provided, set an object, else state = state
   // state = state || {name: 'Anonymouse'};
-  // console.log('New action', action);
+  console.log('New action', action);
   switch (action.type) {
     case 'CHANGE_NAME':
       return {
           ...state,
           name: action.name
       };
+      case 'ADD_HOBBY':
+      return  {
+        ...state,
+        hobbies: [
+          ...state.hobbies,
+          {
+            id: nextHobbyId++,
+            hobby: action.hobby
+          }
+        ]
+      };
+    case 'REMOVE_HOBBY':
+      return {
+        ...state,
+        hobbies: state.hobbies.filter( (hobby) => hobby.id !== action.id )
+      };
+    case 'ADD_MOVIE':
+      return {
+        ...state,
+        movies: [
+          ...state.movies,
+          {
+            id: nextMovieId++,
+            title: action.title,
+            genre: action.genre
+          }
+        ]
+      };
+    case 'REMOVE_MOVIE':
+      return {
+        ...state,
+        movies: state.movies.filter( (movie) => movie.id !== action.id )
+      }
     default:
       return state;
-  }
+    }
 
   return state;
 };
@@ -25,15 +64,17 @@ var store = redux.createStore(reducer, redux.compose(
   window.devToolsExtension ? window.devToolsExtension() : f => f
 ));
 
-// Subscribe to CHANGE_SEARCH_TEXT
+// Subscribe to CHANGE_SEARCH_TEXT    subscribe to dispatches means that on a dispatch or change execute code
 var unsubscribe = store.subscribe( () => {
   var state = store.getState();
 
-  console.log('Name is ', state.name);
+  console.log('Store state: ', state);
   document.getElementById('app').innerHTML = state.name;
 });
 // unsubscribe();
  // console.log('currentState ', store.getState() );
+
+// Make dispatches to the store
 
 store.dispatch({
   // only requirement is type, it resembles the action name
@@ -47,3 +88,46 @@ store.dispatch({
   type: 'CHANGE_NAME',
   name: 'Zorana'
 });
+
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'hiking'
+});
+
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'walking'
+});
+
+store.dispatch({
+  type: 'REMOVE_HOBBY',
+  id: 2
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Lock, stock, and two smoking barrels',
+  genre: 'action'
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Lion King',
+  genre: 'educational'
+});
+
+store.dispatch({
+  type: 'REMOVE_MOVIE',
+  id: 1
+});
+
+/*
+Write a dispatch first before writing the reducer. This way you already conceptualise what needs to happen
+
+
+*/
+
+
+// add movie action
+// call it a few times, only add a title and genre and ID
+// call it a few times
